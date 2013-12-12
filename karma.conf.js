74cbcb46709b425f6a449f2848c138305e5d1ce4
emmaguo <emmahongguo@gmail.com>
2013-12-12 11:42:51 -0800

    Added bower.json.

--- karma.conf.js
+++ karma.conf.js
@@ -14,9 +14,9 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'bower_components/angular/angular.js',
+            'bower_components/angular/angular.min.js',
             'bower_components/angular-mocks/angular-mocks.js',
-            'bower_components/angular-resource/angular-resource.js',
+            'bower_components/angular-resource/angular-resource.min.js',
             'angular-poller.js',
             'test/*.js'
         ],
