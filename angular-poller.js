ebe316f84e20073dc89b893852e7dba2bc2ef608
Emma Guo <emma@lyft.com>
2015-06-10 23:44:01 -0700

    Release v0.3.3.

--- angular-poller.js
+++ angular-poller.js
@@ -5,7 +5,7 @@
  * Angular poller service. It uses a timer and sends requests every few seconds to
  * keep the client synced with the server.
  *
- * @version v0.3.2
+ * @version v0.3.3
  * @link http://github.com/emmaguo/angular-poller
  * @license MIT
  *
