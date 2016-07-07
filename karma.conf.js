cf9b21d9e5ebb0e2e01430b13f783651e3c23e8e
Emma Guo <emmahongguo@gmail.com>
2016-07-06 17:02:52 -0700

    Updated package.json and added .npmignore

--- karma.conf.js
+++ karma.conf.js
@@ -14,11 +14,11 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'bower_components/angular/angular.js',
-            'bower_components/angular-resource/angular-resource.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            'bower_components/lodash/dist/lodash.js',
-            'bower_components/restangular/dist/restangular.js',
+            'node_modules/angular/angular.js',
+            'node_modules/angular-resource/angular-resource.js',
+            'node_modules/angular-mocks/angular-mocks.js',
+            'node_modules/lodash/lodash.js',
+            'node_modules/restangular/dist/restangular.js',
             'angular-poller.js',
             'test/angular-poller-spec.js'
         ],
